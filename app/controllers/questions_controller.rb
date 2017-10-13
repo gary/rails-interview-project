80e5d2a614e476acf83813381af40becb67bb4e9
Gary Iams <ge.iams@gmail.com>
2017-10-13 16:13:50 -0400

    Require every API request to have a valid API key

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,8 +3,25 @@
 # The public REST API for Questions
 # @version 1.0
 class QuestionsController < ApplicationController
+  rescue_from ActionController::ParameterMissing, ActiveRecord::RecordNotFound,
+              with: :tenant_unauthorized
+
+  before_action :authenticate_tenant
+
   # @api
   def index
+    params.require(:api_key)
+
     @questions = Question.not_private
+  end
+
+  private
+
+  def authenticate_tenant
+    @tenant = Tenant.find_by!(api_key: params[:api_key])
+  end
+
+  def tenant_unauthorized
+    render json: { message: 'Invalid API key' }, status: 401
   end
 end
--- spec/requests/question_retrieval_spec.rb
+++ spec/requests/question_retrieval_spec.rb
@@ -2,14 +2,40 @@
 
 require 'rails_helper'
 
+require 'support/database_cleaner'
+require 'support/factory_girl'
+
 RSpec.describe 'Questions API' do
   describe '#index' do
-    it 'returns all non-private Questions' do
-      get '/questions'
+    context 'when the Tenant successfully authenticates' do
+      let!(:tenant) { create(:tenant) }
 
-      expect(response.content_type).to eq('application/json')
-      expect(response).to render_template(:index)
-        .and have_http_status(:ok)
+      it 'returns all non-private Questions' do
+        get '/questions', api_key: tenant.api_key
+
+        expect(response).to render_template(:index)
+          .and have_http_status(:ok)
+      end
+    end
+
+    context 'when the Tenant fails to authenticate' do
+      context 'when they provide an invalid API key' do
+        it 'returns a useful error message' do
+          get '/questions', api_key: 'invalid-api-key'
+
+          expect(response.body).to include('Invalid API key')
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+
+      context 'when they do not provide an API key' do
+        it 'returns a useful error message' do
+          get '/questions'
+
+          expect(response.body).to include('Invalid API key')
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
     end
   end
 end
